76932549ed208c675eb28c14f31e09f733746f1c
yoieh <Yoieh@live.se>
2021-11-23 00:14:13 +0000

    Deploying to gh-pages from @ yoieh/use-signal@81ecd7e31c77aba1d7e84cfa426c3e97aaf31832 🚀

---
+++ precache-manifest.6c6bf03451c354d2af84ad5dbbf1b3e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c23a60ff295fd5a8ee82dbd8916508a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c33ea99662754b8de52c",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "a969af9883459029234c",
+    "url": "./static/js/2.68a6f546.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.68a6f546.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33ea99662754b8de52c",
+    "url": "./static/js/main.7d8e2649.chunk.js"
+  },
+  {
+    "revision": "e1605e4bfd5ab2df1e29",
+    "url": "./static/js/runtime-main.4ba14a44.js"
+  }
+]);
